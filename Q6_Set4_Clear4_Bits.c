fec45635b88aaf78fd9b377d7b9adf863ccf90b8
Loshini08219 <loshinim.ec25@bitsathy.ac.in>
2026-09-19 10:07:24 +0530

    Implement bit manipulation for user input
    The program prompts the user for a value, sets the upper 4 bits, and clears the lower 4 bits of the input value.

---
+++ Q6_Set4_Clear4_Bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned char s;
+
+    printf("Enter a value (0-255): ");
+    scanf("%hhu", &s);
+
+    s=s | 0xF0;
+    s=s & 0xF0;
+
+    printf("After setting the  bit: ");
+
+    for (int i = 7; i >= 0; i--)
+    {
+        printf("%d", (s >> i) & 1);
+    }
+
+    printf("\n");
+
+    return 0;
+}
